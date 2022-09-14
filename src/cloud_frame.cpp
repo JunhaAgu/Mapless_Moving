a21ebd59f6c41ec10189bdb5081a67e8c5cb33cd
jhAgu <wnsgk0264@gmail.com>
2022-09-14 15:09:12 +0900

    image_fill edit.

--- src/cloud_frame.cpp
+++ src/cloud_frame.cpp
@@ -952,21 +952,3 @@ void CloudFrame::reset()
         }
     }
 }
-
-
-
-
-inline float CloudFrame::acos_fast(float x){
-    float negate = float(x < 0);
-    x = abs(x);
-    float ret = -0.0187293;
-    ret = ret * x;
-    ret = ret + 0.0742610;
-    ret = ret * x;
-    ret = ret - 0.2121144;
-    ret = ret * x;
-    ret = ret + 1.5707288;
-    ret = ret * sqrt(1.0-x);
-    ret = ret - 2 * negate * ret;
-    return negate * 3.14159265358979 + ret;
-};
